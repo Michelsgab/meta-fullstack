e12fdbe6f3e3bba797d78b5e2449d222a20b8c48
Michelsgab <eugabmichels@gmail.com>
2022-07-11 22:30:23 -0300

    Header Component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Logo from "../../assets/images/logo.svg";
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="meta__logo__container">
+          <img src={Logo} alt="Meta's Logo" />
+          <h1 className="meta__title">Meta</h1>
+          <a href="https://github.com/Michelsgab/lyrics-search" target="_blank">
+            <i className="bx bxl-github"></i> Gabriel Michels
+          </a>
+        </div>
+      </header>
+    </>
+  );
+}
