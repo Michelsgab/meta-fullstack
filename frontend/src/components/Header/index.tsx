72d46b07bc3674c0c96f7ec463e978c16964bd09
Michelsgab <eugabmichels@gmail.com>
2022-07-11 22:58:18 -0300

    Adjusting Header Link

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export default function Header() {
         <div className="meta__logo__container">
           <img src={Logo} alt="Meta's Logo" />
           <h1 className="meta__title">Meta</h1>
-          <a href="https://github.com/Michelsgab/lyrics-search" target="_blank">
+          <a href="https://github.com/Michelsgab" target="_blank">
             <i className="bx bxl-github"></i> Gabriel Michels
           </a>
         </div>
